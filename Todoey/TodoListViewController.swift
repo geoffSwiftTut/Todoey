667b673d01600f48130c9dca4ad562a37e423c96
Geoff d'Alelio <geoff.dalelio@soabridge.com>
2018-02-20 09:25:35 -0500

    add new item functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-   let itemArray = ["Find Mike", "Buy Eggos","Destroy Demogorgon"]
+   var itemArray = ["Find Mike", "Buy Eggos","Destroy Demogorgon"]
    
    override func viewDidLoad() {
       super.viewDidLoad()
@@ -33,7 +33,7 @@ class TodoListViewController: UITableViewController {
    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     // print(itemArray[indexPath.row])
 
-      //check to se if there is a checkmark already  and turn off, or set the checkmark if it doesn't
+       //check to se if there is a checkmark already  and turn off, or set the checkmark if it doesn't
       if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
          tableView.cellForRow(at: indexPath)?.accessoryType = .none
       } else {
@@ -43,6 +43,31 @@ class TodoListViewController: UITableViewController {
      
    }
    
+   //MARK - Add New Items
    
+   @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+      
+      var textField = UITextField()
+      
+      let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+      //action uses a closure for the process of adding for completion handler
+      let action = UIAlertAction(title: "add Item", style: .default) {(action) in
+         //what will happen once the user clicks the Add Item button
+         
+         //add new item to itemArray
+         self.itemArray.append(textField.text!)
+         //reload the table to display new items
+         self.tableView.reloadData()
+      }  //end of closure handler
+      alert.addTextField { (alertTextField) in
+         alertTextField.placeholder = "What do you want to add?"
+         textField = alertTextField
+      }
+        alert.addAction(action)
+   
+        //show the alert to the user
+         present(alert, animated:true, completion: nil)
+      
+   }
 }
 
